97396d30fc6f5a5dbfc4675eeaaa07492fc2f607
Anastasia <plushvochka@gmail.com>
2022-09-13 16:40:49 +0300

    added example21

---
+++ example19_Palindrome/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая на вход принимает пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Write("Введите целое число ");
+
+void Palindrome (int x1, int x2, int x3, int x4, int x5)
+{if(x1 == x5 & x2 == x4) Console.WriteLine("Это палиндром");
+else Console.WriteLine("Число не является палиндромом");
+}
+
+try
+{
+    int x = Convert.ToInt32(Console.ReadLine());
+    int x1 = x/10000;
+    int x2 = x/1000-x1*10;
+    int x3 = x/100-(x/1000)*10;
+    int x4 = x/10-(x/100)*10;
+    int x5 = x%10;
+
+    Palindrome (x1, x2, x3, x4, x5);
+}
+
+catch
+{
+    Console.WriteLine("Не выполнено условие по вводу корректных данных, попробуйте ввести корректные данные");
+}
---
+++ example21_3D/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая на вход принимает координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+double lengthBetween(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+double xres = Math.Pow(x2 - x1, 2);
+double yres = Math.Pow(y2 - y1, 2);
+double zres = Math.Pow(z2 - z1, 2);
+double res = Math.Sqrt(xres + yres + zres);
+return res;
+}
+
+Console.Write("Введите координату x первой точки ");
+int x1 = Convert.ToInt32 (Console.ReadLine());
+Console.Write("Введите координату y первой точки ");
+int y1 = Convert.ToInt32 (Console.ReadLine());
+Console.Write("Введите координату z первой точки ");
+int z1 = Convert.ToInt32 (Console.ReadLine());
+Console.Write("Введите координату x второй точки ");
+int x2 = Convert.ToInt32 (Console.ReadLine());
+Console.Write("Введите координату y второй точки ");
+int y2 = Convert.ToInt32 (Console.ReadLine());
+Console.Write("Введите координату z второй точки ");
+int z2 = Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine($"Расстояние между точками равно {lengthBetween( x1, y1, z1, x2, y2, z2)}");
